941ba0771a234af0b92bfbf05ac001bc0ae5b02d
Eshiwani Michael <michaelndolo17142@gmail.com>
2025-08-06 14:20:20 +0300

    shop model

---
+++ Backend/Models/Shop.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const shopSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  location: String,{
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+  });
+
+  module.exports = mongoose.model('Shop', shopSchema);
