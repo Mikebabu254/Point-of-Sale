fadbc3b8280f0020ffdcd4c8f874cfa6c0ba2670
Eshiwani Michael <michaelndolo17142@gmail.com>
2025-08-06 14:25:08 +0300

    creating  a user. hash the password before saving

---
+++ Backend/Models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Shop = require('./Shop');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+    },
+    passwordHash: {
+    type: String,
+    required: true
+    },
+    role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'cashier'
+    },
+    Shop: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Shop',
+    required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.methods.isValidPassword = async function(password) {
+    return bcrypt.compareSync(password, this.passwordHash);
+};
+
+module.exports = mongoose.model('User', userSchema);
