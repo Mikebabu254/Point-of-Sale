24d4e66e26ecd548af72efc750ced6ce9f683c78
Eshiwani Michael <michaelndolo17142@gmail.com>
2025-09-01 09:40:34 +0300

    testing if the app is running locally

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-
 const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+ const port = 3000;
 
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use('/api/shops', require('./routes/shopRoutes'));
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
-
-// Root
-app.get('/', (req, res) => {
-  res.send('POS API is running...');
+app.get("/", (req, res) => {
+  res.send("Hello, Express.js is working!");
 });
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(port, () => console.log(`Server running on port ${port}`));
-}).catch(err => {
-  console.error('MongoDB connection error:', err.message);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
