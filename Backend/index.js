c6596c9f481a3fc5c5dca388c34733e0dec91720
Inkognito <michaelndolo17142@gmail.com>
2025-11-20 19:25:55 +0300

    db connection

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,21 @@ dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express.js is working!");
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+  }else{
+    console.log('Connected to the database');
+    connection.release();
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
